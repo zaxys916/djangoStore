621fba22684fd22b0616f2df4a96a643ec84b0b8
zaxys <zaxys@qq.com>
2026-08-06 16:44:32 +0800

    替换User

--- backend/djangoStore/settings.py
+++ backend/djangoStore/settings.py
@@ -219,7 +219,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ##############################
 # 通过提供一个值给AUTH_USER_MODEL设置，指向自定义的模型，Django允许你覆盖默认的User模型：
 # 这个点式路径包含Django应用的名称（必须位于你的INSTALLED_APPS中），和要用作User模型的Django模型的名称。
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
 
 #####CORS#######################
 
