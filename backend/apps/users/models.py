49de893abf639c58e61c3968bdef3282b742d43c
zaxys <zaxys@qq.com>
2026-08-08 09:40:30 +0800

    个人中心的数据展示

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     mobile = models.CharField(max_length=11, unique=True)
+    email_active = models.BooleanField(default=False, verbose_name='邮箱是否激活')
 
     # 解决冲突：添加 related_name
     groups = models.ManyToManyField(
